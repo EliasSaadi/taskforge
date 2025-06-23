df8a932366630d78c9ac61163c299d0f30ca7193
SAADI Elias <eliassaadi13@gmail.com>
2025-06-23 02:09:22 +0200

    feat(route-backend): Ajout d'une route de test

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -11,3 +11,8 @@ Route::post('/login', function () {
     logger('Tentative de login reçue');
     return response()->json(['ok' => true]);
 });
+
+// routes/api.php
+Route::get('/ping', function () {
+    return response()->json(['message' => 'pong']);
+});
