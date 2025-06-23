b4d468d18c39f8b000c6224f08bee2d0676d0378
SAADI Elias <eliassaadi13@gmail.com>
2025-06-23 15:57:35 +0200

    fix(app + route-backend): Correction du .gitignore + Suppresion de la route des logs et ping

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\File;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -11,24 +10,4 @@ Route::get('/user', function (Request $request) {
 Route::post('/login', function () {
     logger('Tentative de login reçue');
     return response()->json(['ok' => true]);
-});
-
-// routes/api.php
-Route::get('/ping', function () {
-    return response()->json(['message' => 'pong']);
-});
-
-
-Route::get('/_debug/logs', function () {
-    $logPath = storage_path('logs/laravel.log');
-
-    if (!File::exists($logPath)) {
-        return response()->json(['error' => 'Log file not found'], 404);
-    }
-
-    $logs = File::get($logPath);
-    $lines = explode("\n", $logs);
-    $lastLines = array_slice($lines, -100);
-
-    return response("<pre>" . implode("\n", $lastLines) . "</pre>")->header('Content-Type', 'text/html');
 });
