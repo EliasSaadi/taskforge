8ff2dbd9074fb2ac24c59b621e92827d34cb497d
SAADI Elias <eliassaadi13@gmail.com>
2025-06-25 00:04:38 +0200

    fix(route-frontend): correction de la variable .env

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
-    "X-Access-Token": import.meta.env.VITE_ACCESS_TOKEN,
+    "X-Access-Token": import.meta.env.VITE_APP_ACCESS_TOKEN,
   },
   withCredentials: true,
 });
